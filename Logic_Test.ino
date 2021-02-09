cb0c6feed09bd5af7ef22eaaea01792ee36a95d5
Abhay2412 <57297897+Abhay2412@users.noreply.github.com>
2021-02-08 19:16:57 -0700

    Updated the Logic Test for the SmartCar so it knows goes in the reverse lap 5 times as well, and need to account for the IMU sensor errors still

---
+++ Logic_Test.ino
@@ -0,0 +1,308 @@
+
+
+/**
+ * Ultrasonic Sensor and Servo Code for the Smart Car
+ * 
+ * Author: Ben, Mutasem, Abhay
+ */
+#include <Wire.h>
+#include <MPU6050.h>
+#include <Servo.h>                  //The servo library gets imported in  
+Servo ultraSonicSensorServo;       //Creating the servo object from the imported library
+MPU6050 gyroAccelTemp;
+
+#define ENA 5
+#define ENB 6
+#define IN1 7
+#define IN2 8
+#define IN3 9
+#define IN4 11
+
+#define GYRO_X_OFFSET 14                     // Determined using IMU_Zero under File > Examples > MPU6050
+#define GYRO_Y_OFFSET -72                    // Determined using IMU_Zero under File > Examples > MPU6050
+#define GYRO_Z_OFFSET 60                     // Determined using IMU_Zero under File > Examples > MPU6050
+
+
+
+const int servoPin = 3;            //Not sure about the pin number 
+
+const int trigPin = A5;           //Trigger Pin of the Ultrasonic Sensor 
+const int echoPin = A4;           //Echo Pin of the Ultrasonic Sensor
+
+long distance;                   //Distance which is measured in cm
+long duration;                   //Duration of the echoPin how long it takes for pulse to travel
+
+  float gyroX =0;
+  float gyroY =0;
+  float gyroZ =0;
+  float gyroBefore = 0;
+unsigned long timer = 0;
+float timeStep = 0.01;
+
+int currentLap =1;
+ int currentLapRev = 1;
+int numberOfLap1 = 2;
+bool forward = true;
+bool reverse = false;
+
+void setup() {
+Serial.begin(38400);            //BaudRate of 38400, and this opens the serial communication 
+
+Wire.begin();
+  Serial.println("Initializing I2C devices...");
+  gyroAccelTemp.initialize();
+
+  // Verify IMU connection
+  Serial.println("Testing device connections...");
+  Serial.println(gyroAccelTemp.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+  
+  gyroAccelTemp.CalibrateGyro();                  // Fully calibrate gyro in about 15 loops
+  gyroAccelTemp.CalibrateAccel();                 // Fully calibrate gyro in about 15 loops
+  Serial.println("Calibration complete");         // Notify when calibration is complete
+  /* The following will NOT get rid of the noise produce by the IMU but they will ensure that
+      the average values are at 0 to a certain extent
+      (Refer to the IMU Error Determination code and the MPU6050 tutorial video for more details)
+  */
+  // Set Gyroscope Offsets
+  gyroAccelTemp.setXGyroOffset(GYRO_X_OFFSET);    // Set the X gyroscope offset
+  gyroAccelTemp.setYGyroOffset(GYRO_Y_OFFSET);    // Set the Y gyroscope offset
+  gyroAccelTemp.setZGyroOffset(GYRO_Z_OFFSET);    // Set the Z gyroscope offset
+
+
+  // IMPORTANT: If you do not calibrate after setting your offset, there will be error
+  gyroAccelTemp.CalibrateGyro(6);                 // Fine Tuning purposes of gyroscope
+  gyroAccelTemp.CalibrateAccel(6);                // Fine Tuning purposes of accelerometer
+  Serial.println("Fine Tuning Complete");         // Notify when fine tuning is complete
+
+ultraSonicSensorServo.attach(servoPin);
+pinMode(IN1, OUTPUT); //Left wheels turn forward
+pinMode(IN3, OUTPUT); //Right wheels turn forward 
+
+pinMode(ENA, OUTPUT); // Enables left wheels to move
+pinMode(ENB, OUTPUT); // Enables right wheels to move
+
+pinMode(trigPin, OUTPUT);       //Setting the trigger pin to output
+pinMode(echoPin, INPUT);       //The echo pin is the input
+ultraSonicSensorServo.write(0);
+ultrasonicDistanceInCM();
+Serial.println(ultrasonicDistanceInCM()+ "is from setup");
+delay(2000);
+setMotorSpeed(100);                                               //Set motor speed 100 out of 255
+moveForward();
+
+}
+
+void loop() {
+
+  timer  =  millis();
+
+  gyroZ = gyroZ + gyroAccelTemp.getRotationZ() / 131.0 *1.76 * timeStep;         // Get current Z axis orientation in degrees (conversion from degrees per second
+
+
+  delay((timeStep*1000) - (millis() - timer));
+  
+  Serial.println();
+  // Print out Gyroscope data
+  Serial.print("Gyroscope (degrees)\t\tZ: ");
+  Serial.println(gyroZ);
+  
+      while(forward){
+      
+             if(ultrasonicDistanceInCM() < 13){                            //If robot is closer than 13cm than turn left slightly
+              wideTurnLeft();  
+              delay(300);
+              moveForward();
+             }
+            
+                else if(ultrasonicDistanceInCM() > 20){ 
+                 wideTurnRight(); 
+                  delay(300);   
+                  moveForward();
+                }
+                if(gyroZ <-360*currentLap){
+                  currentLap++;
+                  
+                }
+                if(currentLap==6){
+                  gyroBefore = gyroZ;
+                  forward = false;
+                  break;
+                }
+            
+               timer  =  millis();
+               
+              gyroZ = gyroZ + gyroAccelTemp.getRotationZ() / 131.0 *10.49* timeStep;         // Get current Z axis orientation in degrees (conversion from degrees per second
+            
+            
+              delay((timeStep*1000) - (millis() - timer));
+              
+                Serial.println();
+                
+              // Print out Gyroscope data
+              Serial.print("Gyroscope (degrees)\t\tZ: ");
+              Serial.println(gyroZ);
+      
+      }
+
+if(currentLap==6){
+  setMaxSpeed();
+  TurnAround();
+  delay(1);
+  stopMovingForever();
+}                                                    
+
+
+    while(reverse){
+          setMotorSpeed(100);                                               //Set motor speed 100 out of 255
+          moveForward();
+           delay(1);
+                 if(ultrasonicDistanceInCM() < 13){                            //If robot is closer than 13cm than turn left slightly
+                  wideTurnRight();  
+                  delay(300);
+                  moveForward();
+                 }
+            
+                else if(ultrasonicDistanceInCM() > 20){ 
+                 wideTurnLeft(); 
+                  delay(300);   
+                  moveForward();
+                }
+                if(gyroZ >(-360*currentLapRev+180)){       //Keep in mind the gyroscope starts at -1800+180 degrees for the reverse
+                  currentLapRev++;               
+                  
+                }
+                if(currentLapRev==6){
+                  gyroBefore = gyroZ;
+                  reverse = false;
+                  break;
+                }
+            
+               timer  =  millis();
+              
+              gyroZ = gyroZ + gyroAccelTemp.getRotationZ() / 131.0 *10.49* timeStep;         // Get current Z axis orientation in degrees (conversion from degrees per second
+            
+            
+              delay((timeStep*1000) - (millis() - timer));
+              Serial.println();
+              // Print out Gyroscope data
+              Serial.print("Gyroscope (degrees)\t\tZ: ");
+              Serial.println(gyroZ);
+            
+      }
+
+if(currentLapRev==6){
+  moveForward();
+  delay(5);
+  if(ultrasonicDistanceInCM() > 40){
+    stopMovingForever();
+    
+  }
+}
+
+
+}
+
+        /////////////
+        /////////////
+        //Functions//
+        /////////////
+        /////////////
+        
+void TurnAround(){                          //Function to turn car around based on gyro data.
+float z = 180;
+      if(gyroZ < (gyroBefore +180)){
+      turnLeft();
+      delay(5);
+      ultraSonicSensorServo.write(180);
+      }
+      
+      if(gyroZ > (gyroBefore +180)){
+      
+        reverse = true;
+        currentLap = 7;                     //Set to 7 so turn around function is ensured to be turned off.
+      }
+
+}
+
+void turnRight()                             // Right turn function
+{
+  digitalWrite(IN1, HIGH);                   // Left forward motion activated
+  digitalWrite(IN2, LOW);                    // Left backward motion not activated
+  digitalWrite(IN3, HIGH);                   // Right backward motion activated
+  digitalWrite(IN4, LOW);                    // Right forward motion not activated
+}
+
+void turnLeft()                              // Left turn function
+{
+  digitalWrite(IN1, LOW);                    // Left forward motion not activated
+  digitalWrite(IN2, HIGH);                   // Left backward motion activated
+  digitalWrite(IN3, LOW);                    // Right backward motion not activated
+  digitalWrite(IN4, HIGH);                   // Right forward motion activated
+
+}
+
+void wideTurnRight()                          // Wide right turn function
+{
+  digitalWrite(IN1, HIGH);                   // Left forward motion not activated
+  digitalWrite(IN2, LOW);                    // Left backward motion activated
+  digitalWrite(IN3, LOW);                    // Right backward motion not activated
+  digitalWrite(IN4, LOW);                    // Right forward motion activated
+
+}
+
+void wideTurnLeft()                          // Wide left turn function
+{
+  digitalWrite(IN1, LOW);                    // Left forward motion not activated
+  digitalWrite(IN2, LOW);                    // Left backward motion activated
+  digitalWrite(IN3, LOW);                    // Right backward motion not activated
+  digitalWrite(IN4, HIGH);                   // Right forward motion activated
+
+}
+void moveForward() {  //Forward function 
+  digitalWrite(IN1, HIGH);  //Left forward motion activated 
+  digitalWrite(IN2, LOW);  //Left backward motion not activated 
+  digitalWrite(IN3, LOW); //Right backward motion not activated 
+  digitalWrite(IN4, HIGH); //Right forward motion activated 
+  
+}
+
+void setMotorSpeed(int newSpeed) {
+  // Avoid setting the motors to 2 different speed as it tends to stop working
+  analogWrite(ENA, newSpeed);               // Enable left motor at set speed
+  analogWrite(ENB, newSpeed);               // Enable right motor at set speed
+}
+
+void setMaxSpeed()                           // Set motors to max speed function
+{
+  digitalWrite(ENA, HIGH);                   // Disable right motor
+  digitalWrite(ENB, HIGH);                   // Disable left motor
+}
+
+
+void stopMoving()                            // Stopping function
+{
+  digitalWrite(IN1, LOW);                    // Left forward motion not activated
+  digitalWrite(IN2, LOW);                    // Left backward motion not activated
+  digitalWrite(IN3, LOW);                    // Right backward motion not activated
+  digitalWrite(IN4, LOW);                    // Right forward motion not activated
+}
+
+void stopMovingForever()                     // Stopping function
+{
+  digitalWrite(ENA, LOW);                    // Disable right motor
+  digitalWrite(ENB, LOW);                    // Disable left motor
+}
+
+
+long ultrasonicDistanceInCM() {               // Returns the distance in centimeters using the Ultrasonic sensor
+  // The above sends a pulse of ultrasonic wave
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);                       // Delay for 2 Microseconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);                      // Delay for 10 Microseconds
+  digitalWrite(trigPin, LOW);
+  // pulseIn waits for the pin to go HIGH, starts timing, then waits for the pin to go LOW and stops timing
+  // pulseIn() return is of type long
+  long duration = pulseIn(echoPin, HIGH);
+  return duration / 29 / 2;
+}
--- Ultrasonic_Sensor/Ultrasonic_Sensor.ino
+++ Ultrasonic_Sensor/Ultrasonic_Sensor.ino
@@ -4,10 +4,17 @@
  * Author: Ben, Mutasem, Abhay
  */
 
-#include <Servo.h>     //The servo library gets imported in 
+#include <Servo.h>                  //The servo library gets imported in  
 Servo ultraSonicSensorServo;       //Creating the servo object from the imported library
+#define ENA 5
+#define ENB 6
+#define IN1 7
+#define IN2 8
+#define IN3 9
+#define IN4 11
 
-const int servoPin = 2;            //Not sure about the pin number 
+
+const int servoPin = 3;            //Not sure about the pin number 
 
 const int trigPin = A5;           //Trigger Pin of the Ultrasonic Sensor 
 const int echoPin = A4;           //Echo Pin of the Ultrasonic Sensor
@@ -19,24 +26,98 @@ void setup() {
 Serial.begin(9600);            //BaudRate of 9600, and this opens the serial communication 
 
 ultraSonicSensorServo.attach(servoPin);
+pinMode(IN1, OUTPUT); //Left wheels turn forward
+pinMode(IN3, OUTPUT); //Right wheels turn forward 
+
+pinMode(ENA, OUTPUT); // Enables left wheels to move
+pinMode(ENB, OUTPUT); // Enables right wheels to move
+
 pinMode(trigPin, OUTPUT);       //Setting the trigger pin to output
 pinMode(echoPin, INPUT);       //The echo pin is the input
+ultraSonicSensorServo.write(0);
+ultrasonicDistanceInCM();
+Serial.println(ultrasonicDistanceInCM()+ "is from setup");
+delay(2000);
+setMotorSpeed(100);
+moveForward();
+
 }
 
 void loop() {
-Serial.println(ultraSonicDistance());  //Calling the method of the distance in cm 
- if (millis() > 10000)
-    ultraSonicSensorServo.write(180); //Turns left at full speed 
-  else if (millis() > 2000)
-    ultraSonicSensorServo.write(0); //Turns right at full speed after 2 seconds
+ /*if (millis() > 7000) //calculate the distance 
+    ultraSonicSensorServo.write(90); //Turns right at full speed 
+    */
+ if(ultrasonicDistanceInCM() < 13){                            //If robot is closer than 13cm than turn left slightly
+  wideTurnLeft();  
+  delay(300);
+  moveForward();
+  Serial.println(ultrasonicDistanceInCM()+ "is from data 13");
+ }
+
+    else if(ultrasonicDistanceInCM() > 20){ 
+     wideTurnRight(); 
+      delay(300);   
+      moveForward();
+      Serial.println(ultrasonicDistanceInCM()+ "is from data 20");
+    }
+}
+void turnRight()                             // Right turn function
+{
+  digitalWrite(IN1, HIGH);                   // Left forward motion activated
+  digitalWrite(IN2, LOW);                    // Left backward motion not activated
+  digitalWrite(IN3, HIGH);                   // Right backward motion activated
+  digitalWrite(IN4, LOW);                    // Right forward motion not activated
+}
+
+void turnLeft()                              // Left turn function
+{
+  digitalWrite(IN1, LOW);                    // Left forward motion not activated
+  digitalWrite(IN2, HIGH);                   // Left backward motion activated
+  digitalWrite(IN3, LOW);                    // Right backward motion not activated
+  digitalWrite(IN4, HIGH);                   // Right forward motion activated
 
 }
-long ultraSonicDistance(){  //Method will return the distance from the ultrasonic sensor 
-  digitalWrite(trigPin, LOW); //Sets it to low so off the trigger pin is at 0V
+
+void wideTurnRight()                          // Wide right turn function
+{
+  digitalWrite(IN1, HIGH);                   // Left forward motion not activated
+  digitalWrite(IN2, LOW);                    // Left backward motion activated
+  digitalWrite(IN3, LOW);                    // Right backward motion not activated
+  digitalWrite(IN4, LOW);                    // Right forward motion activated
+
+}
+
+void wideTurnLeft()                          // Wide left turn function
+{
+  digitalWrite(IN1, LOW);                    // Left forward motion not activated
+  digitalWrite(IN2, LOW);                    // Left backward motion activated
+  digitalWrite(IN3, LOW);                    // Right backward motion not activated
+  digitalWrite(IN4, HIGH);                   // Right forward motion activated
+
+}
+void moveForward() {  //Forward function 
+  digitalWrite(IN1, HIGH);  //Left forward motion activated 
+  digitalWrite(IN2, LOW);  //Left backward motion not activated 
+  digitalWrite(IN3, LOW); //Right backward motion not activated 
+  digitalWrite(IN4, HIGH); //Right forward motion activated 
+  
+}
+
+void setMotorSpeed(int newSpeed) {
+  // Avoid setting the motors to 2 different speed as it tends to stop working
+  analogWrite(ENA, newSpeed);               // Enable left motor at set speed
+  analogWrite(ENB, newSpeed);               // Enable right motor at set speed
+}
+
+long ultrasonicDistanceInCM() {               // Returns the distance in centimeters using the Ultrasonic sensor
+  // The above sends a pulse of ultrasonic wave
+  digitalWrite(trigPin, LOW);
   delayMicroseconds(2);                       // Delay for 2 Microseconds
-  digitalWrite(trigPin, HIGH); //Sets it to high so the trigger pin is at maximum voltagr
+  digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);                      // Delay for 10 Microseconds
-  digitalWrite(trigPin, LOW); //Sets it to low so off the trigger pin is at 0V
-  long duration = pulseIn(echoPin, HIGH); 
-  distance = duration*0.034/ 2; //Using the data sheet
+  digitalWrite(trigPin, LOW);
+  // pulseIn waits for the pin to go HIGH, starts timing, then waits for the pin to go LOW and stops timing
+  // pulseIn() return is of type long
+  long duration = pulseIn(echoPin, HIGH);
+  return duration / 29 / 2;
 }
